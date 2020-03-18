cb6c9a49428417787a2051694ed8d187e603da26
Sara <sara_cerda@hotmail.com>
2020-03-18 12:17:48 -0600

    ft estilos css

--- app/app.js
+++ app/app.js
@@ -5,8 +5,8 @@ const  FormNewTask =() => {
     let container = document.getElementById("my-app");
     let row = `
     <div class= "row">
-        <div class = " col-6 shadow bg-white rounded mt-2">
-            <div class = " form mt-2 ">
+        <div class = " col-6 offset-3 shadow bg-white rounded mt-2">
+            <div class = " form m-2 ">
                 <div class = " form-group "
                     <label for="task"> Tarea: </label>
                     <input type = "text" id = "task" class = "form-control">
@@ -20,6 +20,34 @@ const  FormNewTask =() => {
     `;
     container.innerHTML = row;
 }
+const PrintTaskComplete = () => {
+    let container = document.getElementById("my-app");
+    let row = `<div class = 'row'>`;
+    let data = app.show_done();
+    data.forEach(element =>{
+        row += `
+        <div class = "col-sm-4 mt-2">
+            <div class = "card shadow bg-white rounded">
+                <div class = "card-header">
+                    <h5> Tarea ID ${element.id} </h5>
+                </div>
+                <div class = "card-body">
+                    <p> ${element.task} </p>
+                </div>
+                <div class = "card-footer text-center">
+                    </button>
+                    <button type = "button" onClick= "elimina_tarea(${element.id})" class = "btn btn-danger"> 
+                        Eliminar
+                    </button>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+    row += "</div>";
+    container.innerHTML = row;
+}
+
 const PrintaskActive = () => {
     // Muestra tareas activas
     let container = document.getElementById("my-app");
@@ -36,7 +64,7 @@ const PrintaskActive = () => {
                     <p> ${element.task} </p>
                 </div>
                 <div class = "card-footer text-center">
-                    <button type = "button" class = "btn btn-info"> 
+                    <button type = "button"  onClick ="completa_tarea(${element.id})" class = "btn btn-info"> 
                         Completar
                     </button>
                     <button type = "button" onClick= "elimina_tarea(${element.id})" class = "btn btn-danger"> 
@@ -52,6 +80,10 @@ const PrintaskActive = () => {
 
 }
 /** Funciones */
+function completa_tarea(id=0){
+        app.complete_id(id);
+        PrintaskActive();
+}
 function elimina_tarea(id=0) {
     let opt = confirm("Desea quitar este elemento? ");
     if (opt === true) {
@@ -74,10 +106,14 @@ document.addEventListener("DOMContentLoaded", (success) => {
     console.log("Carga completa:  " + success);
     let nuevo = document.getElementById("new_task");
     let activas = document.getElementById("show_active");
+    let completas = document.getElementById("show_completed");
     nuevo.addEventListener("click", ()=>{
         FormNewTask();
     });
     activas.addEventListener("click", ()=>{
         PrintaskActive();
+    });
+    completas.addEventListener("click", () =>{
+        PrintTaskComplete();
     });
 });
